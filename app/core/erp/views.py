73dd18d0178a4a4e80d792f3cfc00056be29026e
Emmanuel Martinez <juanmartinez9@deloitte.com>
2021-07-18 20:57:09 -0500

    Added first template html

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def myfirstview(request):
-    return HttpResponse('hola esta es mi primera url')
+
+    data = {
+        'name' : 'William'
+    }
+
+    return render(request, 'index.html', data)
