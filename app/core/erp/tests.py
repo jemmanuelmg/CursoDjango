490391f49896d40e72c2acb085cefc539fd8e031
Emmanuel Martinez <juanmartinez9@deloitte.com>
2021-07-18 20:02:06 -0500

    Added final models to the project

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,3 +1,21 @@
+from app.wsgi import *
 from django.test import TestCase
+from core.erp.models import Type
 
-# Create your tests here.
+# Listar
+query = Type.objects.all()
+print(query)
+
+# Insertar
+t = Type()
+t.name = 'Ejemplo'
+t.save()
+
+# Actualizar
+t = Type.objects.get(id=1)
+t.name = 'Accionista'
+t.save()
+
+# Eliminacion
+t = Type.objects.get(id=1)
+t.delete()
